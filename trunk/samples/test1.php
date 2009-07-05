469603c85fb3f7e861f95af0802d1b573a43cbdb
soywiz <soywiz@076a9004-69ab-11de-bb38-9f61fe354615>
2009-07-05 22:31:39 +0000

    - Modified to use namespaces. - Removing the first tab from all the php files.

--- trunk/samples/test1.php
+++ trunk/samples/test1.php
@@ -1,5 +1,5 @@
 <?php
-	require_once(__DIR__ . '/../ste.php');
-	$ste = new ste(__DIR__ . '/templates');
-	$ste->show('extended', __DIR__ . '/templates_cache');
+require_once(__DIR__ . '/../ste.php');
+$ste = new ste\ste(__DIR__ . '/templates');
+$ste->show('extended', __DIR__ . '/templates_cache');
 ?>
