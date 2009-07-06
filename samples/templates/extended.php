9342ab61c0f22ebce95f7b95859afc2f0a24eb83
soywiz <soywiz@076a9004-69ab-11de-bb38-9f61fe354615>
2009-07-06 00:38:10 +0000

    - Added cache_null - 'tag_' -> 'TAG_' - Padding $params to the template - NodeException - node_parser::$tree -> node_parser::$node_root - Added node::checkParams() - Implemented for and foreach. - Bugfixes

--- samples/templates/extended.php
+++ samples/templates/extended.php
@@ -1,5 +1,6 @@
 {!extends name="base"}
 
-{blockdef id=test}{t}HolaHOLA :D{/t}{/blockdef}
-
-{block id=title}Title modified{/block}
+{block id=title}{t}Title modified {$var} a{/t}{/block}
+{block id="empty"}
+	{foreach list="$list" var="$n"}{$n}{/foreach}
+{/block}
