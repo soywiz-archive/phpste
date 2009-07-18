687386724f18a530643c1e78085ed76932a665d4
soywiz <soywiz@076a9004-69ab-11de-bb38-9f61fe354615>
2009-07-18 22:36:33 +0000

    - Moved main source to src folder.

--- samples/test1.php
+++ samples/test1.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/../ste.php');
+require_once(__DIR__ . '/../src/ste.php');
 $ste = new ste\ste(__DIR__ . '/templates', null);
 $ste->show('extended', array(
 	'var' => 'test',
